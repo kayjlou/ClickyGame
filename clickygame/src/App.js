fae27da93d5d750a4abc739c11ba72d583429951
Katie Lewis <klewi314@gmail.com>
2018-07-12 17:58:06 -0700

    Not displaying pictures

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,21 +1,79 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DogCard from "./components/DogCard";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import dogs from "./dogs.json";
+
 
 class App extends Component {
+  state = {
+      message: "Click an image to begin!",
+      topScore: 0,
+      curScore: 0,
+      dogs: dogs,
+      unselectedDogs: dogs
+  }
+
+  componentDidMount() {
+  }
+
+  shuffleArray = array => {
+      for (let i = array.length - 1; i > 0; i--) {
+          let j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+      }
+  }
+
+  selectDog = name => {
+      const findDog = this.state.unselectedDogs.find(item => item.name === name);
+
+      if(findDog === undefined) {
+          // failure to select a new dog
+          this.setState({ 
+              message: "You guessed incorrectly!",
+              topScore: (this.state.curScore > this.state.topScore) ? this.state.curScore : this.state.topScore,
+              curScore: 0,
+              dogs: dogs,
+              unselectedDogs: dogs
+          });
+      }
+      else {
+          // success to select a new dog
+          const newDogs = this.state.unselectedDogs.filter(item => item.name !== name);
+          
+          this.setState({ 
+              message: "You guessed correctly!",
+              curScore: this.state.curScore + 1,
+              dogs: dogs,
+              unselectedDogs: newDogs
+          });
+      }
+
+      this.shuffleArray(dogs);
+  };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      return (
+          <Wrapper>
+         
+          
+              <Header />
+              {
+                  this.state.dogs.map(dog => (
+                      <DogCard
+                          name={dog.name}
+                          image={dog.image}
+                          selectDog={this.selectDog} 
+                          curScore={this.state.curScore}
+                      />
+                  ))
+              }
+          </Wrapper>
+      );
   }
 }
+      
 
 export default App;
